494ce68c881d962b8ae43bc42967ba1cdd67597e
Nikki Wu <nikkiwu9@gmail.com>
2018-11-22 10:01:23 -0800

    Updated SCSS and JS

--- themes/quotes_on_dev/404.php
+++ themes/quotes_on_dev/404.php
@@ -6,25 +6,23 @@
  *
  * @package QOD_Starter_Theme
  */
-
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <section class="error-404 not-found">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php echo esc_html( 'Oops! That page can&rsquo;t be found.' ); ?></h1>
-				</header><!-- .page-header -->
+                <header class="page-header">
+                    <h1 class="page-title"><?php echo esc_html('Oops! That page can&rsquo;t be found.'); ?></h1>
+                </header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try a search?' ); ?></p>
+                <div class="page-content">
+                    <p><?php echo esc_html('It looks like nothing was found at this location. Maybe try a search?'); ?></p>
+                    <?php get_search_form(); ?>
+                </div><!-- .page-content -->
 
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+            </section><!-- .error-404 -->
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php get_footer(); ?>
--- themes/quotes_on_dev/js/custom.js
+++ themes/quotes_on_dev/js/custom.js
@@ -1,14 +1,74 @@
 (function ($) {
+
     $(document).ready(function () {
+        let lastPage = '';
         $('#new-quote-button').on('click', function (event) {
-            console.log('click');
             event.preventDefault();
-        });
+            lastPage = document.URL;
 
+            $.ajax({
+
+                method: 'GET',
+                url: qod_vars.rest_url + 'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+
+                success: function (data) {
+                    const post = data.shift();
+
+                    $('.entry-content').html(post.content.rendered);
+                    $('.entry-title').html(post.title.rendered);
+
+                    if (post._qod_quote_source !== '' && post._qod_quote_source_url !== '') {
+                        $('.source').html(', <a href="' + post._qod_quote_source_url + '" target="_blank">' + post._qod_quote_source + '</a>');
+                    }
+
+                    else if (post._qod_quote_source !== '' && post._qod_quote_source_url === '') {
+                        $('.source').html(', ' + post._qod_quote_source);
+                    }
+
+                    else {
+                        $('.source').html('');
+                    }
+
+
+                    history.pushState(null, null, qod_vars.home_url + '/' + post.slug);
+
+                }
+
+            });
+
+            $(window).on('popstate', function () {
+                window.location.replace(lastPage);
+            });
+
+
+            $('quote-submission-form').on('submit', function (event) {
+                event.preventDefault();
+                postQuote();
+
+
+            });
+
+            function postQuote() {
+                $.ajax({
+                    method: 'POST',
+                    url: qod_vars.rest_url + 'wp/v2/posts',
+                    data: {
+                        title: 'quoteTitle',
+                    },
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader('X-WP-Nonce', qod_vars.nonce);
+                    }
+                }).done(function () {
+                    console.log('response');
+                }).fail(function () {
+                    console.log('hello');
+                });
+
+            }
+
+        }); // end of doc ready
     })
-
 })(jQuery);
-
 
 
 
